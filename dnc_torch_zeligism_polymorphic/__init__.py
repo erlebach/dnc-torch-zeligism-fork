ad5dd64ece3240a27b5c87d39aa1d5df2dd4e527
erlebach <gordon.erlebach@gmail.com>
2025-03-09 11:19:28 -0400

    Simplify imports

---
+++ dnc_torch_zeligism_polymorphic/__init__.py
@@ -0,0 +1,5 @@
+from .memory_adapted  import Memory_Adapted
+from .interface_adapted import DNC_InterfaceLayer_Adapted
+from .dnc_adapted import DNC_Adapted
+
+__all__ = ["Memory_Adapted", "DNC_Adapted", "Interface_Adapted"]
